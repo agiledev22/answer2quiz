4a6e7d0faf47bd7a8c5c5336da6d664b4d0d98ec
ronald0220 <ronaldbucayan20@gmail.com>
2021-01-10 22:08:17 -0800

    update 02

--- qa-plugin/manage-by-excel/qa-plugin.php
+++ qa-plugin/manage-by-excel/qa-plugin.php
@@ -1,30 +1,24 @@
 <?php
   /*
-    Plugin Name: Tag Descriptions Tutorial
+    Plugin Name: Manage By Excel
     Plugin URI:
     Plugin Description: Allows tag descriptions to be displayed as tooltips
     Plugin Version: 1.0
-    Plugin Date: 2016-01-02
+    Plugin Date: 2021-01-10
     Plugin Author:
     Plugin Author URI:
     Plugin License: GPLv2
     Plugin Minimum Question2Answer Version: 1.5
     Plugin Update Check URI:
   */
-
   if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
     header('Location: ../../');
     exit;
   }
-  qa_register_plugin_module(
-    'widget', // type of module
-    'qa-tag-desc-widget.php', // PHP file containing module class
-    'qa_tag_descriptions_widget', // module class name in that PHP file
-    'Tag Descriptions' // human-readable name of module
-  );
+
   qa_register_plugin_module(
     'page', // type of module
-    'qa-tag-desc-edit.php', // PHP file containing module class
-    'qa_tag_descriptions_edit_page', // name of module class
-    'Tag Description Edit Page' // human-readable name of module
+    'qa-upload-excel.php', // PHP file containing module class
+    'qa_upload_excel_page', // name of module class
+    'Upload Excel File' // human-readable name of module
   );
